3f89a12545ef643f31d2e93d8b27f651ea1df710
Daniel Kuc <danielpiotrkuc@gmail.com>
2022-02-12 15:44:25 +0000

    Add project files.

---
+++ TheExchangeApi/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TheExchangeApi.Controllers
+{
+    [ApiController]
+    [Route("test")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet("one")]
+        public string Get()
+        {
+            return "Hello from API";
+        }
+    }
+}
