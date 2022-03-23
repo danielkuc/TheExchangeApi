bbd67c4f0958e46d1edeb837b79042c8b46d489a
Daniel Kuc <danielpiotrkuc@gmail.com>
2022-03-23 15:36:07 +0000

    Added JWT authorisation and authentication in Program.cs

--- TheExchangeApi/Program.cs
+++ TheExchangeApi/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
 var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
@@ -16,6 +18,24 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+//JWT validation, middleware intercepts and validates received requests
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+     .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, c =>
+     {
+         c.Authority = $"https://{builder.Configuration["Auth0:Domain"]}";
+         c.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+         {
+             ValidAudience = builder.Configuration["Auth0:Audience"],
+             ValidIssuer = $"{builder.Configuration["Auth0:Domain"]}"
+         };
+     });
+//authorisation, makes sure JWT has the required scope
+builder.Services.AddAuthorization(o =>
+{
+    o.AddPolicy("todo:read-write", p => p.
+        RequireAuthenticatedUser().
+        RequireClaim("scope", "todo:read-write"));
+});
 
 var app = builder.Build();
 
