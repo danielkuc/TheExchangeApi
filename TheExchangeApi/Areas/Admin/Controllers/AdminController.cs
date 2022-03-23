154b0da3bd13ecb0c6ff28c7632170465a27205c
Daniel Kuc <danpkuc@gmail.com>
2022-03-23 23:10:38 +0000

    aded DB connection

--- TheExchangeApi/Areas/Admin/Controllers/AdminController.cs
+++ TheExchangeApi/Areas/Admin/Controllers/AdminController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using TheExchangeApi.Areas.Services;
+using TheExchangeApi.Models;
 
 namespace TheExchangeApi.Areas.Admin.Controllers
 {
@@ -8,6 +10,13 @@ namespace TheExchangeApi.Areas.Admin.Controllers
     [ApiController]
     public class AdminController : ControllerBase
     {
+        private readonly ProductService _productService;
+
+        public AdminController(ProductService productService)
+        {
+            _productService = productService;
+        }
+
         [EnableCors("myFrontendPolicy")]
         [HttpGet]
         public string Get()
@@ -17,5 +26,12 @@ namespace TheExchangeApi.Areas.Admin.Controllers
 
 
         //crud Product
+        [EnableCors("myFrontendPolicy")]
+        [HttpPost]
+        public ActionResult<Product> Create(Product product)
+        {
+            _productService.Create(product);
+            return CreatedAtRoute("GetProduct", product);
+        }
     }
 }
--- TheExchangeApi/Areas/Admin/Services/ProductService.cs
+++ TheExchangeApi/Areas/Admin/Services/ProductService.cs
@@ -8,14 +8,24 @@ namespace TheExchangeApi.Areas.Services
 {
     public class ProductService
     {
-        private readonly MongoCollectionBase<Product> _products;
+        private readonly IMongoCollection<Product> _products;
         
         public ProductService()
         {
             var ConnectionString = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetSection("ProductsDbSettings")["ConnectionString"];
             var DbName = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetSection("ProductsDbSettings")["DbName"];
+            var CollectionName = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetSection("ProductsDbSettings")["CollectionName"];
+
             var client = new MongoClient(ConnectionString);
             var database = client.GetDatabase(DbName);
+
+            _products = database.GetCollection<Product>(CollectionName);
+        }
+
+        public Product Create(Product product)
+        {
+            _products.InsertOne(product);
+            return product;
         }
     }
 }
