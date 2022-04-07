24cd6b05c6631add6284d622fd0570032fcd5441
Daniel Kuc <danielpiotrkuc@gmail.com>
2022-04-07 15:29:43 +0100

    Added DeactivateProductController and placeholders for ActivateProduct

---
+++ TheExchangeApi/Areas/Admin/Products/ActivateProduct/ActivateProduct.cs
@@ -0,0 +1,6 @@
+﻿namespace TheExchangeApi.Areas.Admin.Products.ActivateProduct
+{
+    public class ActivateProduct
+    {
+    }
+}
---
+++ TheExchangeApi/Areas/Admin/Products/DeactivateProduct/DeactivateProductController.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using TheExchangeApi.Models;
+
+namespace TheExchangeApi.Areas.Admin.Products.DeactivateProduct
+{
+    [Route("admin/product.deactivate")]
+    [ApiController]
+    [EnableCors("myFrontendPolicy")]
+    public class DeactivateProductController : ControllerBase
+    {
+        private readonly ISender _mediator;
+
+        public DeactivateProductController(ISender mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> DeactivateProduct(string Id, bool IsActive)
+        {
+            var deactivatedProduct = _mediator.Send(new DeactivateProduct.DeactivateProductCommand(Id, IsActive));
+            return Ok(deactivatedProduct);
+        }
+    }
+}
