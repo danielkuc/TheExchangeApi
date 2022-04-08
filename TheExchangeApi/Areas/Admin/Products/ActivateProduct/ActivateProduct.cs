c5f0b857bfd02b18f916ff9832b4283fac5ea6e7
Daniel Kuc <danielpiotrkuc@gmail.com>
2022-04-08 21:59:17 +0100

    finished ActicateProduct

--- TheExchangeApi/Areas/Admin/Products/ActivateProduct/ActivateProduct.cs
+++ TheExchangeApi/Areas/Admin/Products/ActivateProduct/ActivateProduct.cs
@@ -1,6 +1,34 @@
-﻿namespace TheExchangeApi.Areas.Admin.Products.ActivateProduct
+﻿using MediatR;
+using MongoDB.Driver;
+using TheExchangeApi.Models;
+
+namespace TheExchangeApi.Areas.Admin.Products.ActivateProduct
 {
     public class ActivateProduct
     {
+        public record ActivateProductCommand(string Id) : IRequest<UpdateResult>;
+
+        public class ActivateProductHandler : IRequestHandler<ActivateProductCommand, UpdateResult>
+        {
+            private readonly IProductDatabaseSettings _settings;
+            private readonly IMongoClient _client;
+
+            public ActivateProductHandler(IProductDatabaseSettings settings, IMongoClient client)
+            {
+                _settings = settings;
+                _client = client;
+            }
+
+            public Task<UpdateResult> Handle(ActivateProductCommand request, CancellationToken cancellationToken)
+            {
+                var productDatabase = _client.GetDatabase(_settings.DatabaseName);
+                var filterById = Builders<Product>.Filter.Eq(product => product.Id, request.Id);
+                var activateProduct = Builders<Product>.Update.Set(p => p.IsAvailable, true);
+                var updateProduct = productDatabase.GetCollection<Product>(_settings.ProductsCollectionName)
+                    .UpdateOneAsync(filterById, activateProduct);
+
+                return updateProduct;
+            }
+        }
     }
 }
