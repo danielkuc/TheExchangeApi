f52d2aa8efeeb5023243dde28fe33132c8faf0e9
Daniel Kuc <danielpiotrkuc@gmail.com>
2022-04-22 08:29:03 +0100

    Added get by ID and get all products into SHop area

---
+++ TheExchangeApi/Areas/Shop/Products/GetAllProducts/GetAllProducts.cs
@@ -0,0 +1,34 @@
+﻿using MediatR;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using TheExchangeApi.Models;
+
+namespace TheExchangeApi.Areas.Shop.Products.GetAllProducts
+{
+    public class GetAllProducts
+    {
+        public record GetAllProductsQuery : IRequest<List<Product>>;
+
+        public class GetAllProductsQueryHandler : IRequestHandler<GetAllProductsQuery, List<Product>>
+        {
+            private readonly IProductDatabaseSettings _settings;
+            private readonly IMongoClient _client;
+
+            public GetAllProductsQueryHandler(IProductDatabaseSettings settings, IMongoClient client)
+            {
+                _settings = settings;
+                _client = client;
+            }
+
+            public Task<List<Product>> Handle(GetAllProductsQuery request, CancellationToken cancellationToken)
+            {
+                var db = _client.GetDatabase(_settings.DatabaseName);
+
+                var products = db.GetCollection<Product>(_settings.ProductsCollectionName)
+                    .Find(new BsonDocument()).ToList(cancellationToken: cancellationToken);
+
+                return Task.FromResult(products);
+            }
+        }
+    }
+}
---
+++ TheExchangeApi/Areas/Shop/Products/GetProductById/GetProductById.cs
@@ -0,0 +1,36 @@
+﻿using MongoDB.Driver;
+using MediatR;
+using TheExchangeApi.Models;
+
+namespace TheExchangeApi.Areas.Shop.Products.GetProductById
+{
+    public class GetProductById
+    {
+        public record GetProductByIdQuery(string Id) : IRequest<Product>;
+
+        public class GetProductByIdHandler : IRequestHandler<GetProductByIdQuery, Product>
+        {
+            private readonly IProductDatabaseSettings _settings;
+            private readonly IMongoClient _client;
+
+            public GetProductByIdHandler(IProductDatabaseSettings settings, IMongoClient client)
+            {
+                _settings = settings;
+                _client = client;
+            }
+
+            public Task<Product> Handle(GetProductByIdQuery request, CancellationToken cancellationToken)
+            {
+                var productDatabase = _client.GetDatabase(_settings.DatabaseName);
+
+                var filterById = Builders<Product>.Filter.Eq(product => product.Id, request.Id);
+
+                var firstFoundProduct = productDatabase.GetCollection<Product>(_settings.ProductsCollectionName)
+                    .Find(filterById).FirstOrDefault(cancellationToken: cancellationToken);
+
+                return Task.FromResult(firstFoundProduct);
+            }
+        }
+
+    }
+}
