10a27995a1a90fdf097de29236b706628455a8ca
Daniel Kuc <danpkuc@gmail.com>
2022-04-28 21:32:08 +0100

    added optional parameters to  query

--- TheExchangeApi/Areas/Admin/Products/FindManyProducts/FindManyProductsController.cs
+++ TheExchangeApi/Areas/Admin/Products/FindManyProducts/FindManyProductsController.cs
@@ -17,7 +17,7 @@ namespace TheExchangeApi.Areas.Admin.Products.FindManyProducts
         }
 
         [HttpGet]
-        public async Task<IActionResult> FindManyProducts(string name="")
+        public async Task<IActionResult> FindManyProducts(string? name = null, string? category = null, string? priceFrom = null, double? priceTo = null)
         {
             var products = await _mediator.Send(new FindManyProducts.FindManyProductsQuery(name));
             return Ok(products);
