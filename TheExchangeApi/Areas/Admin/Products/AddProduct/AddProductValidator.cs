90f69887d832d8c83e5c0dbbb23d77d434e4ab30
Daniel Kuc <danielpiotrkuc@gmail.com>
2022-05-11 15:33:02 +0100

    minor validation change

--- TheExchangeApi/Areas/Admin/Products/AddProduct/AddProductValidator.cs
+++ TheExchangeApi/Areas/Admin/Products/AddProduct/AddProductValidator.cs
@@ -9,10 +9,11 @@ namespace TheExchangeApi.Areas.Admin.Products.AddProduct
             RuleFor(c => c.PassedProduct.Name)
                 .NotEmpty()
                 .Length(3,20)
-                .WithMessage($"Product name should be between 3 and 20 characters");
+                .WithMessage("Product name should be between 3 and 20 characters");
             RuleFor(c => c.PassedProduct.Description)
                 .NotEmpty()
-                .Length(5, 25);
+                .Length(5, 25)
+                .WithMessage("Product description should be between 5 and 25 characters");
             RuleFor(c => c.PassedProduct.Price)
                 .NotEmpty(); 
         }
