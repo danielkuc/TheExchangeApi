8cbfa22bd6da52a4046e9c59fb75d75ba7e6a0b8
Daniel Kuc <danpkuc@gmail.com>
2022-05-12 20:24:19 +0100

    Refactored Program.cs - removed comments and placed builder services in descriptive functions

--- TheExchangeApi/Program.cs
+++ TheExchangeApi/Program.cs
@@ -5,100 +5,107 @@ using MongoDB.Driver;
 using TheExchangeApi.Models;
 using Microsoft.IdentityModel.Tokens;
 using System.Security.Claims;
-using System.Net;
-using Microsoft.AspNetCore.Diagnostics;
 using FluentValidation;
 using TheExchangeApi.PipelineBehaviours;
 
 var builder = WebApplication.CreateBuilder(args);
-// Add services to the container.
 
-// Add MediatR and register pipeline behaviours
-builder.Services.AddMediatR(typeof(Program).Assembly)
-            .AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>))
-            .AddTransient(typeof(IPipelineBehavior<,>), typeof(LoggingBehaviour<,>));
+RegisterMediatR();
 
-//register validators
-builder.Services.AddValidatorsFromAssembly(typeof(Program).Assembly);
+RegisterValidators();
 
-// get section "ProductDatabase" from appsettings.json
-builder.Services.Configure<ProductDatabaseSettings>(
-    builder.Configuration.GetSection("ProductDatabase"));
+GetDatabaseName();
 
-//dependency injection: whenever IProductDatabaseSettings is required, provide instance of ProductDatabaseSettings class 
-builder.Services.AddSingleton<IProductDatabaseSettings>(serviceProvider =>
-    serviceProvider.GetRequiredService<IOptions<ProductDatabaseSettings>>().Value);
+InjectDatabaseSettings();
 
-//provide IMongoClient with ConnectionString from appsettings.json
-builder.Services.AddSingleton<IMongoClient>(singleton =>
-    new MongoClient(builder.Configuration.GetValue<string>("ProductDatabase:ConnectionString")));
+GetConnectionStringForMongoClient();
 
-
-// CORS configuration, defined a CORS policy to use with attributes for each controlle/method.
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("theExchangeShopPolicy",
-        policy =>
-        {
-            policy.WithOrigins(
-                "https://exchange-shop.netlify.app",
-                "http://localhost:3000",
-                "https://exchange-dashboard.netlify.app")
-            .AllowAnyHeader();
-        }   
-    );
-});
+CorsConfig();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-//JWT validation, middleware intercepts and validates received requests
-builder.Services.AddAuthentication(options =>
+JwtAuthentication();
+AddAuthPolicies();
+
+
+void RegisterMediatR()
 {
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(options =>
+    builder.Services.AddMediatR(typeof(Program).Assembly)
+            .AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>))
+            .AddTransient(typeof(IPipelineBehavior<,>), typeof(LoggingBehaviour<,>));
+}
+void RegisterValidators()
 {
-    options.Authority = "https://the-exchange.eu.auth0.com";
-    options.Audience = "https://exchange/api";
-    options.TokenValidationParameters = new TokenValidationParameters
+    builder.Services.AddValidatorsFromAssembly(typeof(Program).Assembly);
+}
+void GetDatabaseName()
+{
+    builder.Services.Configure<ProductDatabaseSettings>(
+    builder.Configuration.GetSection("ProductDatabase"));
+}
+void InjectDatabaseSettings()
+{
+    builder.Services.AddSingleton<IProductDatabaseSettings>(serviceProvider =>
+    serviceProvider.GetRequiredService<IOptions<ProductDatabaseSettings>>().Value);
+}
+void GetConnectionStringForMongoClient()
+{
+    builder.Services.AddSingleton<IMongoClient>(singleton =>
+    new MongoClient(builder.Configuration.GetValue<string>("ProductDatabase:ConnectionString")));
+}
+void CorsConfig()
+{
+    builder.Services.AddCors(options =>
     {
-        NameClaimType = ClaimTypes.NameIdentifier
-    };
-});
-
-builder.Services.AddAuthorization(options => { 
-    options.AddPolicy("ReadAccess", policy => policy.RequireClaim("permissions", "read:products"));
-    options.AddPolicy("WriteAccess", policy => policy.RequireClaim("permissions", "write:products"));
-});
-
+        options.AddPolicy("theExchangeShopPolicy",
+            policy =>
+            {
+                policy.WithOrigins(
+                    "https://exchange-shop.netlify.app",
+                    "http://localhost:3000",
+                    "https://exchange-dashboard.netlify.app")
+                .AllowAnyHeader();
+            }
+        );
+    });
+}
+void JwtAuthentication()
+{
+    builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    }).AddJwtBearer(options =>
+    {
+        options.Authority = "https://the-exchange.eu.auth0.com";
+        options.Audience = "https://exchange/api";
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            NameClaimType = ClaimTypes.NameIdentifier
+        };
+    });
+}
+void AddAuthPolicies()
+{
+    builder.Services.AddAuthorization(options => {
+        options.AddPolicy("ReadAccess", policy => policy.RequireClaim("permissions", "read:products"));
+        options.AddPolicy("WriteAccess", policy => policy.RequireClaim("permissions", "write:products"));
+    });
+}
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-//global exception handler
-app.UseExceptionHandler(options =>
-{
-    options.Run(async context =>
-    {
-        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
-        var exception = context.Features.Get<IExceptionHandlerFeature>();
-        if (exception != null)
-        {
-            await context.Response.WriteAsync(exception.Error.Message);
-        }
-    });
-});
 
+app.UseExceptionHandler("/error");
 
-//calling CORS service initialiser
 app.UseCors();
 app.UseHttpsRedirection();
 
