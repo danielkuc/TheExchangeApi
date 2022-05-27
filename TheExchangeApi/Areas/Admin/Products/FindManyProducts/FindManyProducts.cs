8d139735d89f823e02c12d4ff457ae955d60a824
Daniel Kuc <danielpiotrkuc@gmail.com>
2022-05-27 08:28:05 +0100

    refactored find many products

--- TheExchangeApi/Areas/Admin/Products/FindManyProducts/FindManyProducts.cs
+++ TheExchangeApi/Areas/Admin/Products/FindManyProducts/FindManyProducts.cs
@@ -1,8 +1,6 @@
 ﻿using MediatR;
 using MongoDB.Driver;
-using MongoDB.Bson;
 using TheExchangeApi.Models;
-using System.Text.RegularExpressions;
 
 namespace TheExchangeApi.Areas.Admin.Products.FindManyProducts
 {
@@ -22,31 +20,16 @@ namespace TheExchangeApi.Areas.Admin.Products.FindManyProducts
 
             public Task<Response> Handle(Request request, CancellationToken cancellationToken)
             {
-                //var filter = Builders<Product>.Filter.Eq(product => product.IsAvailable, true);
                 var query = _collection.AsQueryable().Where(p => p.IsAvailable);
 
                 if (!string.IsNullOrWhiteSpace(request.Name))
-                {
-                    //var nameFilter = Builders<Product>.Filter.
-                    //    Regex("name", new BsonRegularExpression(new Regex(request.Name, RegexOptions.IgnoreCase)));
-                    //filter &= nameFilter;
                     query = query.Where(p => p.Name.Contains(request.Name));
-                }
 
                 if (request.PriceFrom.HasValue)
-                {
-                    //var convertedPriceFrom = Convert.ToDouble(request.PriceFrom);
-                    //var convertedPriceTo = Convert.ToDouble(request.PriceTo);
-                    //var priceFilter = Builders<Product>.Filter.Gt("price",convertedPriceFrom) & Builders<Product>.Filter.Lt("price", convertedPriceTo);
-                    //filter &= priceFilter;
                     query = query.Where(p => p.Price >= request.PriceFrom);
-                }
 
                 if (request.PriceTo.HasValue)
-                {
                     query = query.Where(p => p.Price <= request.PriceTo);
-                }
-                //var products = _collection.Find(filter).ToList(cancellationToken: cancellationToken);
 
                 return Task.FromResult(new Response(query.ToList()));
             }
