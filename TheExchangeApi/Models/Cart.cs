830e4ded8b30c08abbd1b40c10f3c1667fdae2c5
Daniel Kuc <danpkuc@gmail.com>
2022-06-02 21:58:58 +0100

    created cart and cart item model

---
+++ TheExchangeApi/Models/Cart.cs
@@ -0,0 +1,9 @@
+﻿namespace TheExchangeApi.Models
+{
+    public class Cart
+    {
+        public string CartId { get; set; }
+        public DateTime DateCreated { get; set; }
+        public List<CartItem> Items { get; set; }
+    }
+}
---
+++ TheExchangeApi/Models/CartItem.cs
@@ -0,0 +1,9 @@
+﻿namespace TheExchangeApi.Models
+{
+    public class CartItem
+    {
+        public string CartId { get; set; }
+        public int QuantityReserved { get; set; }
+        public  Product Product { get; set; }
+    }
+}
