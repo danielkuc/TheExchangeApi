0a02ca33b0bf1e7f6afac67d786ca23ef8fd7a57
Daniel Kuc <danielpiotrkuc@gmail.com>
2022-07-28 21:53:59 +0100

    added request and response in Checkout

---
+++ TheExchangeApi/Models/Cart/Checkout.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using MediatR;
+
+namespace TheExchangeApi.Models.Cart
+{
+    public class Checkout
+    {
+        public class Request : IRequest<Response>
+        {
+            public ShoppingCart Cart {get; set;}
+        }
+
+        public class Response
+        {
+            public Guid OrderId { get; set; }
+        }
+    }
+}
