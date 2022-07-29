c8767492c42700aa8a3ccff8448e3163da623c0c
Daniel Kuc <danpkuc@gmail.com>
2022-07-29 23:03:16 +0100

    started implementing cart controller

---
+++ TheExchangeApi/Areas/Shop/Cart/CartController.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Driver;
+using TheExchangeApi.Models;
+using MediatR;
+using MongoDB.Bson;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TheExchangeApi.Areas.Shop.Cart
+{
+    public class CartController : BaseShopController
+    {
+        private readonly IMongoCollection<Product> _collection;
+        private readonly IMediator _mediator;
+
+        public CartController(IMongoCollection<Product> collection, IMediator mediator)
+        {
+            _collection = collection;
+            _mediator = mediator;
+        }
+
+        public async Task<IActionResult> AddItem(ObjectId id)
+        {
+            var product = _collection.AsQueryable().First(p => p.Id == id);
+            throw new NotImplementedException();
+        }
+    }
+}
