bec8588bb4411a84fa14e19790e7413798470040
Daniel Kuc <danielpiotrkuc@gmail.com>
2022-08-10 15:28:26 +0100

    started UpdateProductPrice

---
+++ TheExchangeApi/Areas/Admin/Products/UpdateProductPrice/UpdateProductPrice.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using MongoDB.Driver;
+using TheExchangeApi.Models;
+using Polly;
+
+namespace TheExchangeApi.Areas.Admin.Products.UpdateProductPrice
+{
+    public class UpdateProductPrice
+    {
+        public record Request(Product ProductToUpdate) :IRequest<Response>;
+        public record Response;
+    }
+}
---
+++ TheExchangeApi/Areas/Admin/Products/UpdateProductPrice/UpdateProductPriceController.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using static TheExchangeApi.Areas.Admin.Products.UpdateProductPrice.UpdateProductPrice;
+
+namespace TheExchangeApi.Areas.Admin.Products.UpdateProductPrice
+{
+    [Route("admin/product.update.name")]
+    public class UpdateProductPriceController : AccessController
+    {
+        [HttpPost]
+        public async Task<Response> Action(
+            [FromBody] Request request,
+            [FromServices] ISender mediator,
+            CancellationToken cancellationToken
+            ) => await mediator.Send(request, cancellationToken);
+
+
+    }
+}
