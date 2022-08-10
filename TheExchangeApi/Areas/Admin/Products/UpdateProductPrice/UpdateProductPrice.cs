491e743f584647bac09c9c58b12770b458239547
Daniel Kuc <danpkuc@gmail.com>
2022-08-10 19:17:42 +0100

    Finished UpdateProductPrice

--- TheExchangeApi/Areas/Admin/Products/UpdateProductPrice/UpdateProductPrice.cs
+++ TheExchangeApi/Areas/Admin/Products/UpdateProductPrice/UpdateProductPrice.cs
@@ -9,5 +9,33 @@ namespace TheExchangeApi.Areas.Admin.Products.UpdateProductPrice
     {
         public record Request(Product ProductToUpdate) :IRequest<Response>;
         public record Response;
+        public class RequestHandler : IRequestHandler<Request, Response>
+        {
+            private readonly IMongoCollection<Product> _collection;
+
+            public RequestHandler(IMongoCollection<Product> collection)
+            {
+                _collection = collection;
+            }
+            public async Task<Response> Handle(Request request, CancellationToken cancellationToken)
+            {
+                var dbProduct = _collection.Find(p => p.Id == request.ProductToUpdate.Id).SingleAsync();
+                var retryPolicy = Policy.Handle<Exception>().Retry(retryCount: 3);
+
+                if (request.ProductToUpdate.Version != dbProduct.Result.Version)
+                {
+                    throw new Exception($"Failed to update document. Database version='{dbProduct.Result.Version}' Current version='{request.ProductToUpdate.Version}'");
+                }
+
+                var NewVersion = Guid.NewGuid();
+
+                _collection.UpdateOne(p => p.Id == request.ProductToUpdate.Id, Builders<Product>
+                                            .Update
+                                            .Set(p => p.Price, request.ProductToUpdate.Price)
+                                            .Set(p => p.Version, NewVersion));
+
+                return await Task.FromResult(new Response());
+            }
+        }
     }
 }
