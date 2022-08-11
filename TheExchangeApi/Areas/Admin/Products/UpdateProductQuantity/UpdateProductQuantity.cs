1d36a6a2dbc9bcc6bbfc01261811d7d26a42f630
Daniel Kuc <danielpiotrkuc@gmail.com>
2022-08-11 15:47:00 +0100

    finished update product quantity

---
+++ TheExchangeApi/Areas/Admin/Products/UpdateProductQuantity/UpdateProductQuantity.cs
@@ -0,0 +1,40 @@
+﻿using MediatR;
+using MongoDB.Driver;
+using TheExchangeApi.Models;
+using Polly;
+
+namespace TheExchangeApi.Areas.Admin.Products.UpdateProductQuantity
+{
+    public class UpdateProductQuantity
+    {
+        public record Request(Product ProductToUpdate) : IRequest<Response>;
+        public record Response();
+        public class RequestHandler : IRequestHandler<Request, Response>
+        {
+            private readonly IMongoCollection<Product> _collection;
+
+            public RequestHandler(IMongoCollection<Product> collection)
+            {
+                _collection = collection;
+            }
+            public Task<Response> Handle(Request request, CancellationToken cancellationToken)
+            {
+                var dbProduct = _collection.Find(p => p.Id == request.ProductToUpdate.Id).SingleAsync();
+                var retryPolicy = Policy.Handle<Exception>().Retry(retryCount: 3);
+
+                if (request.ProductToUpdate.Version != dbProduct.Result.Version)
+                {
+                    throw new Exception($"Failed to update document. Database version='{dbProduct.Result.Version}' Current version='{request.ProductToUpdate.Version}'");
+                }
+
+                var NewVersion = Guid.NewGuid();
+
+                _collection.UpdateOne(p => p.Id == request.ProductToUpdate.Id, Builders<Product>
+                                            .Update
+                                            .Set(p => p.Quantity, request.ProductToUpdate.Quantity)
+                                            .Set(p => p.Version, NewVersion));
+                return Task.FromResult(new Response());
+            }
+        }
+    }
+}
---
+++ TheExchangeApi/Areas/Admin/Products/UpdateProductQuantity/UpdateProductQuantityController.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using static TheExchangeApi.Areas.Admin.Products.UpdateProductQuantity.UpdateProductQuantity;
+
+namespace TheExchangeApi.Areas.Admin.Products.UpdateProductQuantity
+{
+    [Route("admin/product.update.quantity")]
+    public class UpdateProductQuantityController : AccessController
+    {
+        [HttpPost]
+        public async Task<Response> Action(
+            [FromBody] Request request,
+            [FromServices] ISender mediator,
+            CancellationToken cancellationToken
+            ) => await mediator.Send(request, cancellationToken);
+
+
+    }
+}
