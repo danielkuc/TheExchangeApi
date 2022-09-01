34896dce4353e1b2236b546735f20be8166c5dc5
Daniel Kuc <danpkuc@gmail.com>
2022-09-01 21:31:39 +0100

    Added remove product

---
+++ TheExchangeApi/Areas/Shop/Cart/RemoveProduct/RemoveProduct.cs
@@ -0,0 +1,44 @@
+﻿using MediatR;
+using TheExchangeApi.Models;
+using MongoDB.Driver;
+
+namespace TheExchangeApi.Areas.Shop.Cart.RemoveProduct
+{
+    public class RemoveProduct
+    {
+        public record Request(Product product) : IRequest<Response>;
+        public record Response;
+        public class RequestHandler : IRequestHandler<Request, Response>
+        {
+            private readonly IMongoCollection<Product> _productCollection;
+            private readonly IMongoCollection<ShoppingCart> _cartCollection;
+            private readonly IHttpContextAccessor _httpContextAccessor;
+
+            public RequestHandler(
+                IMongoCollection<Product> productCollection
+                , IMongoCollection<ShoppingCart> cartCollection
+                , IHttpContextAccessor accessor
+                )
+            {
+                _productCollection = productCollection;
+                _cartCollection = cartCollection;
+                _httpContextAccessor = accessor;
+            }
+            public async Task<Response> Handle(Request request, CancellationToken cancellationToken)
+            {
+                var productFromDb = _productCollection.AsQueryable().Where(x => x.Id == request.product.Id).Single();
+                if (_httpContextAccessor.HttpContext.Session.GetString("CartId") == null)
+                {
+                    throw new ArgumentNullException("Cart doesn't exist");
+                }
+                var cartId = _httpContextAccessor.HttpContext.Session.GetString("CartId");
+                var cartFromDB = _cartCollection.AsQueryable().Where(c => c.Id == cartId).Single();
+                cartFromDB.Products.Remove(request.product.Id);
+                await _cartCollection
+                    .ReplaceOneAsync(c => c.Id == cartId, cartFromDB, cancellationToken: cancellationToken);
+                return await Task.FromResult(new Response());
+            }
+        }
+:
+    }
+}
---
+++ TheExchangeApi/Areas/Shop/Cart/RemoveProduct/RemoveProductController.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using static TheExchangeApi.Areas.Shop.Cart.RemoveProduct.RemoveProduct;
+
+namespace TheExchangeApi.Areas.Shop.Cart.RemoveProduct
+{
+    [Route("shop/cart.product.remove")]
+    public class RemoveProductQuantityController : BaseShopController
+    {
+        [HttpPost]
+        public async Task<Response> Action(
+            [FromBody] Request request,
+            [FromServices] ISender mediator,
+            CancellationToken cancellationToken
+            ) => await mediator.Send(request, cancellationToken);
+
+
+    }
+}
