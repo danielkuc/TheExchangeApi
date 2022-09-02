e58a36ddd056c753b0465835154b0046e76b16c9
Daniel Kuc <danpkuc@gmail.com>
2022-09-02 22:17:07 +0100

    scaffolded checkouc model

--- TheExchangeApi/Models/Checkout.cs
+++ TheExchangeApi/Models/Checkout.cs
@@ -1,26 +1,15 @@
 ﻿using System;
+using System.Linq;
 using MediatR;
 
 namespace TheExchangeApi.Models
 {
     public class Checkout
     {
-        public class Request : IRequest<Response>
-        {
-            public ShoppingCart Cart {get; set;}
-        }
-
-        public class Response
-        {
-            public Guid OrderId { get; set; }
-        }
-
+        public record Request(ShoppingCart Cart) : IRequest<Response>;
+        public record Response(Guid OrderId);
         public class Handler : IRequestHandler<Request, Response>
         {
-            public Handler()
-            {
-
-            }
             public Task<Response> Handle(Request request, CancellationToken cancellationToken)
             {
                 throw new NotImplementedException();
